f1f7fab28a5a77919b77cbe690fb53dae690eb59
Rahul Agrawal <rahul.agrawal.con@hotstar.com>
2022-02-15 03:18:26 +0530

    removed unnecessary code

--- components/PlaylistItem/index.tsx
+++ components/PlaylistItem/index.tsx
@@ -5,16 +5,13 @@ interface PlayListItemProps {
   item: Data,
 }
 
-function PlayListItem({ item } : PlayListItemProps) {
+function PlayListItem({ item }: PlayListItemProps) {
   const { id, images, name, description } = item;
-  const [draggedElementStyle, setDraggedElementStyle] = React.useState('');
 
   return (
     <div
-      data-id={id}
       key={id}
-      draggable={true}
-      className={`flex gap-2 py-1 mb-2 border-b border-gray-600 cursor-pointer ${draggedElementStyle}`}
+      className="flex gap-2 py-1 mb-2 border-b border-gray-600 cursor-pointer"
     >
       <img src={images[0].url} width={80} height={80} style={{ objectFit: 'cover' }} className="select-none" alt="playlist thumbnail" />
       <p className='flex flex-col select-none'>
