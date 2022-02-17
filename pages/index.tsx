113e1e44e97c710d9b008c7a5041a5f7315c4cf7
Rahul Agrawal <rahul.agrawal.con@hotstar.com>
2022-02-18 01:03:04 +0530

    added useMemo to avoid rerenders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { InferGetServerSidePropsType } from 'next';
-import React, {useEffect, useMemo} from 'react';
+import React, { useEffect, useMemo } from 'react';
 import Layout from '../components/Layout';
 import PlayLists from '../components/Playlists';
 import { Data } from '../types/Playlist';
@@ -12,15 +12,15 @@ const Home: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (
   const dispatch = useDispatch();
   const myPlayList: Data[] = useSelector((state: RootState) => state.playlist.MyPlaylist);
   const myPlayListIds = myPlayList && myPlayList.map((val) => val.id);
-  const playlist = myPlayListIds ? data.filter((val) => !myPlayListIds.includes(val.id)) : []
+  const playlist = useMemo(() => data.filter((val) => !myPlayListIds.includes(val.id)), [data, myPlayListIds])
 
   useEffect(() => {
-    dispatch({type: t.INITIALIZE_PLAYLIST, name: 'Playlist', payload: { playLists: playlist}})
-    dispatch({type: t.INITIALIZE_PLAYLIST, name: 'MyPlaylist', payload: { playLists: myPlayList }})
+    dispatch({ type: t.INITIALIZE_PLAYLIST, name: 'Playlist', payload: { playLists: playlist } })
+    dispatch({ type: t.INITIALIZE_PLAYLIST, name: 'MyPlaylist', payload: { playLists: myPlayList } })
   }, [data, dispatch, myPlayList, playlist])
 
   const handleOnDragEnd = (result: DropResult) => {
-    dispatch({type: t.MOVE_PLAYLIST, payload: result})
+    dispatch({ type: t.MOVE_PLAYLIST, payload: result })
   }
 
   return (
@@ -45,7 +45,7 @@ export async function getServerSideProps() {
         data
       }
     }
-  } catch(err) {
+  } catch (err) {
     return {
       props: {
         data: []
