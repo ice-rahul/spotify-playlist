8788bd6e9ff19430da5f6167436b00bd08b270a6
Rahul Agrawal <rahul.agrawal.con@hotstar.com>
2022-02-25 12:13:08 +0530

    minor design fix

--- components/Playlists/index.tsx
+++ components/Playlists/index.tsx
@@ -6,7 +6,7 @@ import { Droppable, Draggable } from 'react-beautiful-dnd';
 const PlayLists: React.FC<PlayListsProps> = ({ label = 'Playlist', list = [] }) => {
 
   return (
-    <div className='flex flex-col border rounded text-gray-700 border-gray-700 flex-1 w-3/4 max-w-lg px-4 py-2'>
+    <div className='flex flex-col border rounded text-gray-700 border-gray-700 flex-1 w-full sm:w-3/4 mx-2 max-w-lg px-4 py-2' style={{minHeight: '50px'}}>
       <div className='font-semibold text-xl border-b border-gray-800 mb-2'>{label}</div>
       <Droppable droppableId={`playlistItems-${label.split(' ').join('-')}`} type="Playlist">
         {(provided) => (
